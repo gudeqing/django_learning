6a3378186633f371acbb0ca44b7a385a40877627
deqing.gu <deqing.gu@majorbio.com>
2018-01-25 14:14:27 +0800

    提交新项目

--- djangosite/app/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from app.models import Moment
-
-class MomentForm(ModelForm):
-    class Meta(object):
-        model = Moment
-        fields = '__all__'
--- djangosite/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-# hello danny
-
-urlpatterns = [
-    url(r'moments_input', views.moments_input),
-    url(r'', views.welcome),
-]
